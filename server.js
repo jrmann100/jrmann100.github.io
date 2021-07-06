ee02a0ebccac31f11d4e803be5c7e6c77a55d39b
Jordan Mann <jordaloo@gmail.com>
2021-07-05 18:29:14 -0700

    /routing for testing server to match Pages

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use("/static", express.static("public/static"));
-app.use("/fragments", express.static("public/fragments", { extensions: ["html.inc"], index: "index.html.inc" }));
-app.use(express.static("public/build", { extensions: ["html"], index: "index.html" }));
+app.use("/routing/static", express.static("static"));
+app.use("/routing/fragments", express.static("fragments", { extensions: ["html.inc"], index: "index.html.inc" }));
+app.use("/routing", express.static("build", { extensions: ["html"], index: "index.html" }));
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
+  console.log(`http://localhost:${port}/routing`)
 });
