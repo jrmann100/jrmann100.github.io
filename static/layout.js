29b9d222146c5418a2c94c5ef33a8ce7c8d175e2
Jordan Mann <jordaloo@gmail.com>
2021-11-20 19:29:51 -0500

    Serialize fragments before extracting regions

--- static/layout.js
+++ static/layout.js
@@ -1,52 +1,59 @@
 
-// handle history navigation within the page.
+// handle history navigation (back, forward).
 window.addEventListener("popstate", ev => {
     console.log(`[popstate] ${window.location.pathname}`);
     load(window.location.pathname);
 });
 
-const regionRegex = /<!--{{(?<open>.+)}}-->(?<content>(.|\n)*?)<!--{{(?<close>\/.+)}}-->/i;
+// matches a label comment's content, i.e. {{/label}}.
 const tagRegex = /^{{(\/?.+)}}$/;
 
-
-// this needs to be error-tested and documented. EXTENSIVELY.
-async function load(path) {
-
-    let text = await (await fetch(path.replace(".html", ".html.inc"))).text(); // todo: fallback
-
+// identify regions in a document or fragment by their label comments.
+// the regions themselves do not include the labels, only their enclosed content.
+async function locateRegions(root) {
     const commentsIterator = document.createNodeIterator(
-        document.documentElement,
+        root,
         NodeFilter.SHOW_COMMENT,
         { acceptNode(node) { return node.nodeValue.match(tagRegex) !== null ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT } }
     );
-    let docRegions = {};
+
+    // label: Range
+    let regions = {};
+
     let start, end;
-    for (
-        [start, end] = [commentsIterator.nextNode(), commentsIterator.nextNode()];
-        start !== null && end !== null;
-        [start, end] = [commentsIterator.nextNode(), commentsIterator.nextNode()]) {
+
+    // take matching labels two at a time and convert them into ranges enclosing content.
+    while (JSON.stringify([start, end] = [commentsIterator.nextNode(), commentsIterator.nextNode()]) !== "[null,null]") {
         const label = start.nodeValue.match(tagRegex)[1];
         if ("/" + label !== end.nodeValue.match(tagRegex)[1]) throw new Error("document's region tags don't match!", start, end);
         const range = document.createRange();
         range.setStartAfter(start);
         range.setEndBefore(end);
-        range.deleteContents();
-        if (label === "main") updateMainHeight(); // todo
-        docRegions[label] = range;
-    }
+        regions[label] = range;
+    };
+    return regions;
+}
+// load a page fragment for a given path and swap the current content with its own.
+// todo test with malformed data
+async function load(path) {
 
+    // fetch the fragment text.
+    let text = await (await fetch(path.replace(".html", ".html.inc"))).text(); // todo fallback
+    // turn the fragment into DOM content.
+    const contextualFragment = dynamify(document.createRange().createContextualFragment(text));
 
-    let region;
-    while ((region = regionRegex.exec(text)) !== null) {
-        text = text.substring(0, region.index) + text.substring(region.index + region[0].length);
-        if (!("/" + region.groups?.open === region.groups?.close)) throw new Error("fragment's region tags don't match!", region.groups?.open, region.groups?.close);
-        const label = region.groups?.open;
-        console.log("rendering region", region.groups?.open);
-        if (!Reflect.has(docRegions, label)) throw new Error("could not find document region for fragment region", label);
-        docRegions[label].insertNode(dynamify(docRegions[label].createContextualFragment(region.groups?.content)));
-        if (label === "main") updateMainHeight(); // todo
-    }
-    if (text.length < 0) console.warn("Template content found out of a region:", text);
+    // find existing document regions on the page.
+    const docRegions = await locateRegions(document.documentElement);
+
+    // clear all document regions.
+    Object.values(docRegions).forEach(range => range.deleteContents());
+
+    // match the new fragment regions to cleared document regions with the same label.
+    const fragmentRegions = await locateRegions(contextualFragment);
+    Object.entries(fragmentRegions).forEach(([label, range]) => {
+                if (!Reflect.has(docRegions, label)) throw new Error("could not find document region for fragment region", label);
+                docRegions[label].insertNode(range.cloneContents());
+    });
 }
 
 // convert hard links into dynamic ones that load() content instead of redirecting.
@@ -71,32 +78,3 @@ function dynamify(parent) {
 
 // make all links dynamic, enabling routing.
 dynamify(document.body);
-
-// todo fadeout content without frame
-async function asyncAnimate(el, frames) {
-    return new Promise(res => setTimeout(res,
-        el.animate(
-            frames, {
-            duration: 500,
-            fill: "forwards",
-            easing: "ease-out"
-        }
-        ).effect.getComputedTiming().duration));
-}
-
-const fadeout = [{
-    opacity: "1",
-},
-{
-    opacity: "0",
-}];
-
-const fadein = [{
-    opacity: "0",
-},
-{
-    opacity: "1",
-}];
-
-function updateMainHeight() { document.querySelector("main#layout").style.setProperty("--main-height", document.querySelector("main#layout").getBoundingClientRect().height + "px"); }
-updateMainHeight();
