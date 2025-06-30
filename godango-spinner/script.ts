21ae948a066c176716aba0fcd77a43af509694c1
Jordan Mann <jordaloo@gmail.com>
2025-06-30 11:02:29 -0700

    feat: minor optimizations

--- godango-spinner/script.ts
+++ godango-spinner/script.ts
@@ -1,35 +1,38 @@
 const reels = [...document.querySelectorAll<HTMLDivElement>(".reel")].map(
-  (reel) => [
-    reel.querySelector<HTMLDivElement>(".face:nth-child(1)"),
-    reel.querySelector<HTMLDivElement>(".face:nth-child(2)"),
-  ]
+  (reel) => {
+    const a = reel.querySelector<HTMLDivElement>(".face:nth-child(1)");
+    const b = reel.querySelector<HTMLDivElement>(".face:nth-child(2)");
+    if (a === null || b === null) {
+      throw new Error("Reel faces not found");
+    }
+    return [a, b];
+  }
 );
 
-const betweenFactory =
-  (progress: number) =>
-  (...stops: number[]) => {
-    const position = progress * (stops.length - 1);
-    const lowerValue = stops[Math.floor(position)];
-    const upperValue = stops[Math.ceil(position)];
-    return lowerValue + (upperValue - lowerValue) * (position % 1);
-  };
+const between = (progress: number, ...stops: number[]) => {
+  const position = progress * (stops.length - 1);
+  const lowerValue = stops[Math.floor(position)];
+  const upperValue = stops[Math.ceil(position)];
+  return lowerValue + (upperValue - lowerValue) * (position % 1);
+};
 
 const update = (element: HTMLElement, progress: number, a = false) => {
-  const between = betweenFactory((progress + Number(a) * 0.5) % 1);
+  const p = (progress + (a ? 0 : 0.5)) % 1;
   element.style.transform = `translateY(${
-    between(-100, 0, 100) - Number(!a) * 100
-  }%) rotateX(${between(50, -50)}deg)`;
-  element.style.transformOrigin = `${between(100, 0)}% 50%`;
-  element.style.opacity = `${between(0, 1, 0)}`;
+    between(p, -100, 0, 100) - Number(!a) * 100
+  }%) rotateX(${between(p, 50, -50)}deg)`;
+  element.style.transformOrigin = `${between(p, 100, 0)}% 50%`;
+  element.style.opacity = `${between(p, 0, 1, 0)}`;
 };
 
 const velocities = reels.map(() => 0);
 const progresses = reels.map(() => 0);
+
 let lastTimestamp = 0;
 const tick = (timestamp: number) => {
+  const timeDelta = timestamp - lastTimestamp;
   reels.forEach(([a, b], i) => {
-    progresses[i] =
-      (progresses[i] + (timestamp - lastTimestamp) * velocities[i]) % 1;
+    progresses[i] = (progresses[i] + timeDelta * velocities[i]) % 1;
 
     if (velocities[i] < 1e-4) {
       // if velocity is low or negative, bring it back to zero.
@@ -38,15 +41,10 @@ const tick = (timestamp: number) => {
       velocities[i] = 0;
     } else {
       // apply friction
-      velocities[i] *= 0.9; // + 0.04 * (i / reels.length);
+      velocities[i] *= 0.9;
     }
 
-    const snaps = [0, 0.5, 1];
-    const nearestSnap = snaps.reduce((prev, curr) =>
-      Math.abs(curr - progresses[i]) < Math.abs(prev - progresses[i])
-        ? curr
-        : prev
-    );
+    const nearestSnap = Math.round(progresses[i] * 2) / 2;
     // the spring can only engage if the velocity is low enough;
     // otherwise it glides across the peaks.
     if (velocities[i] < 0.002) {
@@ -58,9 +56,6 @@ const tick = (timestamp: number) => {
       progresses[i] = nearestSnap;
     }
 
-    if (a === null || b === null) {
-      throw new Error("Reel faces not found");
-    }
     update(a, progresses[i], true);
     update(b, progresses[i], false);
   });
@@ -70,7 +65,6 @@ const tick = (timestamp: number) => {
 };
 
 document.body.addEventListener("click", () => {
-  // v += 0.005;
   for (let i = 0; i < reels.length; i++) {
     setTimeout(() => {
       velocities[i] += 0.005;
