25d02dc95d5e96fa84dbb5147884d60cfae14f24
Jordan Mann <jordaloo@gmail.com>
2025-07-01 22:34:13 -0700

    feat: docs, and weaker directional bias

--- godango-spinner/index.js
+++ godango-spinner/index.js
@@ -1,28 +1,83 @@
+/**
+ * Container element for the reels.
+ */
 const machine = document.querySelector(".machine");
 if (machine === null) {
   throw new Error("Machine element not found");
 }
 
-const reels = [...machine.querySelectorAll(".reel")].map((reel) => {
-  const [a, b] = reel.querySelectorAll(".face");
+/**
+ * The reels of the machine.
+ */
+const reels = [...machine.querySelectorAll(".reel")];
+
+/**
+ * The controller reel is the first reel.
+ *
+ * It has a static label and handles user input.
+ */
+const controller = reels[0];
+
+/**
+ * The faces of the machine, grouped in pairs by reel.
+ */
+const faces = reels.map((reel) => {
+  const faces = [...reel.querySelectorAll(".face")];
+  if (faces.length !== 2) {
+    throw new Error("Each reel must have exactly two faces");
+  }
+  const [a, b] = faces;
   return [a, b];
 });
 
-const controller = machine.querySelector(".controller");
-if (controller === null) {
-  throw new Error("Controller element not found");
-}
-
-const reelCount = reels.length;
-const velocities = new Array(reelCount).fill(0);
-const positions = new Array(reelCount).fill(0);
-let movingReels = 0;
-let lastWheelTime = -Infinity;
-let lastTickTime = -Infinity;
-let isTicking = false;
+/**
+ * The total number of reels, including the controller.
+ */
+const reelCount = faces.length;
 
 /**
- * Array of tuples containing the time the last reel was boosted and the index of the next reel to boost.
+ * The velocity of each reel.
+ * @type {number[]}
+ */
+const velocities = new Array(reelCount).fill(0);
+
+/**
+ * The position of each reel, between 0 and 1.
+ * The faces swap positions such that there is always at least one face visible,
+ * and another face entering from the top.
+ *
+ * | Position | First Face | Second Face |
+ * |----------|------------|-------------|
+ * | 0        | Center     | Above       |
+ * | 0.49     | Below      | Entering    |
+ * | 0.5      | Top        | Center      |
+ * | 0.99     | Entering   | Below       |
+ */
+const positions = new Array(reelCount).fill(0);
+
+/**
+ * The number of reels currently moving.
+ * The same as counting nonzero velocities.
+ */
+let movingReels = 0;
+
+/**
+ * The time the last wheel event was triggered, or -Infinity if no wheel event has occurred.
+ */
+let lastWheelTime = -Infinity;
+
+/**
+ * The time the last animation frame was processed, or -Infinity if no frames have been processed.
+ */
+let lastFrameTime = -Infinity;
+
+/**
+ * Whether the animation loop is currently running.
+ */
+let animationRunning = false;
+
+/**
+ * Queue of tuples containing the time the last reel was boosted and the index of the next reel to boost.
  * @type {[number, number][]}
  */
 const clicks = [];
@@ -105,7 +160,6 @@ const clearVelocity = (reelIndex) => {
   }
 };
 
-// TODO: a and b are swapped in these functions!
 /**
  * Update the position of a reel face.
  * @param {HTMLElement} face the element to update.
@@ -114,7 +168,8 @@ const clearVelocity = (reelIndex) => {
  */
 const renderFace = (face, position, a = false) => {
   // the position of the second face is offset by 0.5
-  const p = Math.abs((position + (a ? 0.5 : 0)) % 1);
+  // occasionally the position will be negative; then we should add 1, mod, then abs.
+  const p = Math.abs((position + 1 + (a ? 0.5 : 0)) % 1);
   face.style.transform = `translateY(${
     // go from all the way above the reel to all the way below it
     between(p, -100, 0, 100) - Number(!a) * 100
@@ -128,9 +183,8 @@ const renderFace = (face, position, a = false) => {
  * @param {number} i the index of the reel to update.
  * @param {number} newPosition the new position of the reel, between 0 and 1.
  */
-// todo: call with "force" to update the first time
 const updatePosition = (i, newPosition) => {
-  const [a, b] = reels[i];
+  const [a, b] = faces[i];
   if (i !== 0) {
     if (newPosition > 1) {
       b.textContent = String.fromCharCode(65 + Math.floor(Math.random() * 26));
@@ -143,104 +197,106 @@ const updatePosition = (i, newPosition) => {
   renderFace(b, positions[i], false);
 };
 
-/**
- *
- * @param {number} timestamp
- */
-const tick = (timestamp) => {
-  if (!isTicking) return;
+const START_OFFSET = 50;
+const END_OFFSET = 50;
 
-  const timeDelta = (timestamp - lastTickTime) / 1000;
+/**
+ * Update the state of the machine until all reels have stabilized.
+ *
+ * @type {FrameRequestCallback}
+ */
+const handleAnimationFrame = (timestamp) => {
+  if (!animationRunning) return;
+
+  // limit the time delta to avoid large jumps;
+  // e.g., if the page was momentarily inactive.
+  const timeDelta = Math.min((timestamp - lastFrameTime) / 1000, 0.1);
   const timeFactor = timeDelta * 60;
   const frictionFactor = Math.pow(0.9, timeFactor);
 
   for (let i = 0; i < clicks.length; i++) {
-    // immediately start the control reel and the first reel.
+    // immediately boost the control reel and the first reel.
     if (clicks[i][1] === 0) {
       addVelocity(clicks[i][1]++, 5);
       addVelocity(clicks[i][1]++, 5);
       clicks[i][0] = timestamp;
     } else {
-      // todo: replace all magic numbers
-      if (timestamp - clicks[i][0] > 50) {
+      // for every following reel, wait START_OFFSET ms after the last reel was boosted.
+      if (timestamp - clicks[i][0] > START_OFFSET) {
         clicks[i][0] = timestamp;
         addVelocity(clicks[i][1]++, 5);
       }
     }
+    // remove this entry if there are no more reels to boost.
+    // clicks is a queue and all clicks take the same amount of time to process,
+    // so the entry to remove is always the first one.
     if (clicks[i][1] >= reelCount) {
       clicks.shift();
       i--;
     }
   }
 
-  reels.forEach((_, i) => {
-    if (velocities[i] < 0) {
-      // if velocity is negative, bring it back to zero.
-      // checkme: there is probably a more elegant way to do this!
-      // like having the spring apply a weaker backwards force in the first place.
-      clearVelocity(i);
-    } else {
-      // apply friction
-      scaleVelocity(i, frictionFactor);
-    }
+  faces.forEach((_, i) => {
+    // apply friction, and resist backward motion.
+    scaleVelocity(i, frictionFactor * (velocities[i] < 0 ? 0.6 : 1));
 
-    if (timestamp - lastWheelTime > (i === 0 ? 1 : i) * 50) {
+    // manual movement (scrolling) overrides the spring.
+    // once there has been 50ms of no manual movement,
+    // the springs start to engage one at a time;
+    // except for the controller (0th) reel, which engages at the same time as the first reel.
+    if (timestamp - lastWheelTime > (i === 0 ? 1 : i) * END_OFFSET) {
       const nearestSnap = Math.round(positions[i] * 2) / 2;
       // the spring can only engage if the velocity is low enough;
       // otherwise it glides across the peaks.
       if (velocities[i] < 2) {
         addVelocity(i, (nearestSnap - positions[i]) * 8 * timeFactor);
       }
-
-      if (Math.abs(velocities[i]) < 1e-2) {
-        clearVelocity(i);
-        positions[i] = nearestSnap;
-      }
     }
 
-    const newPosition = positions[i] + timeDelta * velocities[i];
-    updatePosition(i, newPosition);
+    updatePosition(i, positions[i] + timeDelta * velocities[i]);
   });
 
-  lastTickTime = timestamp;
+  lastFrameTime = timestamp;
 
-  // If all reels have stopped moving and none are held, stop the loop.
+  // if all reels have stopped moving and none are held, stop the loop.
   if (movingReels === 0 && timestamp - lastWheelTime > reelCount * 50) {
-    isTicking = false;
+    animationRunning = false;
   } else {
-    requestAnimationFrame(tick);
+    requestAnimationFrame(handleAnimationFrame);
   }
 };
 
-const startTicking = () => {
-  if (!isTicking) {
-    isTicking = true;
-    // Reset timestamp to avoid a large jump after a pause
-    lastTickTime = performance.now();
-    requestAnimationFrame(tick);
+/**
+ * Re-start the animation loop if it was paused.
+ */
+const resumeAnimation = () => {
+  if (!animationRunning) {
+    animationRunning = true;
+    // reset timestamp to avoid a large jump after a pause
+    lastFrameTime = performance.now();
+    requestAnimationFrame(handleAnimationFrame);
   }
 };
 
 controller.addEventListener("click", () => {
   clicks.push([-Infinity, 0]);
-  startTicking();
+  resumeAnimation();
 });
 
+// TODO: add touch support.
 controller.addEventListener("wheel", (event) => {
   event.preventDefault();
   if (event.deltaY >= 0) {
     return;
   }
-  if (!isTicking) {
-    startTicking();
-  }
+  // safe to call every time, there's an embedded check to prevent multiple RAF calls.
+  resumeAnimation();
   lastWheelTime = performance.now();
+  // manually scroll the reels.
   for (let i = 0; i < reelCount; i++) {
     updatePosition(i, Math.max(positions[i] - event.deltaY / 1000, 0));
   }
 });
 
 // quickly init the reels - should just loop once and then terminate since there is no motion.
-startTicking();
-
-// TODO: add touch support.
+resumeAnimation();
