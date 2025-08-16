ba6a5d6d681c319b6d4f4322acd924b412f3d39a
Jordan Mann <jordaloo@gmail.com>
2025-08-16 10:01:35 -0400

    feat: split tabs

--- static/components/tabs/tabs.js
+++ static/components/tabs/tabs.js
@@ -2,9 +2,22 @@ import Tab from './tab/tab.js';
 
 export default class Tabs extends HTMLElement {
   /**
-   * @type {Tab[]}
+   * TODO: descriptions should be required on class properties
+   * @type {Map<Tab, HTMLElement>}
    */
-  tabs;
+  tabLabels;
+
+  /**
+   * TODO
+   * @type {Tab}
+   */
+  selectedTab;
+
+  /**
+   * TODO
+   * @type {HTMLElement}
+   */
+  wrapper;
 
   /**
    * @type {(els: Element[]) => asserts els is Tab[] }
@@ -44,6 +57,8 @@ export default class Tabs extends HTMLElement {
       throw new Error('Could not find wrapper element for tabs component');
     }
 
+    this.wrapper = wrapper;
+
     const header = templateContent.querySelector('.tabs-header');
 
     if (!(header instanceof HTMLElement)) {
@@ -52,19 +67,42 @@ export default class Tabs extends HTMLElement {
 
     const tabs = Array.from(this.children);
     Tabs.tryCoerceTabs(tabs);
-    this.tabs = tabs;
 
-    header.replaceChildren(
-      ...tabs.map((tab) => {
+    this.tabLabels = new Map(
+      tabs.map((tab) => {
         // TODO: semantic tagging
-        const label = document.createElement('div');
+        const label = document.createElement('button');
+        label.classList.add('no-ia');
         label.textContent = tab.label;
-        return header.appendChild(label);
+        label.dataset.label = tab.label;
+        label.addEventListener('click', () => this.select(tab));
+        return [tab, label];
       })
     );
 
-    wrapper.replaceChildren(...this.children);
+    // TODO: initially selected tab
+    this.select(tabs[0]);
+
+    header.replaceChildren(...this.tabLabels.values());
 
     this.replaceChildren(templateContent);
+  }
+
+  /**
+   * TODO
+   * @param {Tab} tab
+   */
+  select(tab) {
+    if (!this.tabLabels.has(tab)) {
+      throw new Error('Can only select tabs that are children of this Tabs component!');
+    }
+    if (this.selectedTab === tab) {
+      return;
+    }
+    this.tabLabels.get(this.selectedTab)?.setAttribute('aria-selected', 'false');
+    this.selectedTab = tab;
+    this.tabLabels.get(tab)?.setAttribute('aria-selected', 'true');
+
+    this.wrapper.replaceChildren(tab);
   }
 }
